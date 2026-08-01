ae0917c32e7019cd422423e98c9e6cfd81e91f04
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-01 23:58:10 +0200

    ci: publish flake inputs to Attic

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "db-harbor - generic database-operation plans and NixOS systemd wiring";
 
   inputs = {
-    rs-harbor.url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=c26b735eede8078f795651c4a9cbf0be8733b221";
+    rs-harbor.url = "github:caniko/rs-harbor/e2778ff3beca1bd4c1f5183313251d1fb5b46dd6";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     crane.url = "github:ipetkov/crane";
   };
@@ -123,6 +123,21 @@
     in {
       default = pkgs.mkShell {inherit packages;};
       docs = pkgs.mkShell {inherit packages;};
+    });
+
+    apps = forAllSystems ({pkgs, ...}: let
+      atticAdapter = rs-harbor.lib.mkAdapter {
+        attic = {
+          endpoint = "https://attic.candee.baby";
+          cache = "canix";
+        };
+      };
+    in {
+      push-flake-inputs = rs-harbor.lib.mkAtticPush {
+        inherit pkgs;
+        adapter = atticAdapter;
+        flake = ".";
+      };
     });
   };
 }
